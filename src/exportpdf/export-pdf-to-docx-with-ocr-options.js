2099eb74f73f7a85219f891bf27f84386d779a8b
Jasnoor Singh <singh.jasnoor2104@gmail.com>
2022-11-15 16:40:27 +0530

    Removing function params
    Signed-off-by: Jasnoor Singh <singh.jasnoor2104@gmail.com>

--- src/exportpdf/export-pdf-to-docx-with-ocr-options.js
+++ src/exportpdf/export-pdf-to-docx-with-ocr-options.js
@@ -37,10 +37,12 @@ try {
     const options = new exportPDF.options.ExportPDFOptions(exportPDF.options.ExportPDFOptions.OCRSupportedLocale.EN_US);
     exportPDFOperation.setOptions(options);
 
+    //Generating a file name
+    let outputFilePath = createOutputFilePath();
+
     // Execute the operation and Save the result to the specified location.
     exportPDFOperation.execute(executionContext)
-        .then(result => result.saveAsFile(createOutputFileDirectoryPath("output/ExportPDFWithOCROptions",
-            "Export","pdf")))
+        .then(result => result.saveAsFile(outputFilePath))
         .catch(err => {
             if (err instanceof PDFServicesSdk.Error.ServiceApiError
                 || err instanceof PDFServicesSdk.Error.ServiceUsageError) {
@@ -51,12 +53,12 @@ try {
         });
 
     //Generates a string containing a directory structure and file name for the output file.
-    function createOutputFileDirectoryPath(directory, name, format) {
+    function createOutputFilePath() {
         let date = new Date();
         let dateString = date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" +
             ("0" + date.getDate()).slice(-2) + "T" + ("0" + date.getHours()).slice(-2) + "-" +
             ("0" + date.getMinutes()).slice(-2) + "-" + ("0" + date.getSeconds()).slice(-2);
-        return (directory + '/' + name + '_' + dateString + '.' + format);
+        return ("output/ExportPDFWithOCROptions/export" + dateString + ".docx");
     }
 
 } catch (err) {
