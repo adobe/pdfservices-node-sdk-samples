62327a620255432b98adca8a8e73c92041403428
Varun Kumar <noreply@noreply.com>
2023-05-15 12:39:27 +0530

    Autotag Template

---
+++ src/autotag/autotag-pdf-with-options.js
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2023 Adobe
+ * All Rights Reserved.
+ *
+ * NOTICE: Adobe permits you to use, modify, and distribute this file in
+ * accordance with the terms of the Adobe license agreement accompanying
+ * it. If you have received this file from a source other than Adobe,
+ * then your use, modification, or distribution of it requires the prior
+ * written permission of Adobe.
+ */
+
+const PDFServicesSdk = require('@dcloud/pdfservices-node-sdk');
+
+/**
+ * This sample illustrates how to generate a tagged PDF along with a report and shift the headings in
+ * the output PDF file.
+ * <p>
+ * Refer to README.md for instructions on how to run the samples.
+ */
+
+try {
+    // Initial setup, create credentials instance.
+    const credentials =  PDFServicesSdk.Credentials
+        .serviceAccountCredentialsBuilder()
+        .fromFile("pdfservices-api-credentials.json")
+        .build();
+
+    // Create an ExecutionContext using credentials and create a new operation instance.
+    const executionContext = PDFServicesSdk.ExecutionContext.create(credentials),
+        autotagPDF = PDFServicesSdk.AutotagPDF,
+        autotagPDFOperation = autotagPDF.Operation.createNew();
+
+    // Set operation input from a source file.
+    const input = PDFServicesSdk.FileRef.createFromLocalFile('resources/autotagPDFInput.pdf');
+    autotagPDFOperation.setInput(input);
+
+    // Create Options
+    const autotagPDFOptions = new PDFServicesSdk.AutotagPDF.options.AutotagPDFOptions.Builder()
+        .shiftHeadings()
+        .generateReport()
+        .build();
+
+    // Set operation options
+    autotagPDFOperation.setOptions(autotagPDFOptions);
+
+    //Generating a file name
+    let outputTaggedFilePath = createOutputFilePath(".pdf");
+    let outputReportFilePath = createOutputFilePath(".xlsx");
+
+    // Execute the operation and Save the result to the specified location.
+    autotagPDFOperation.execute(executionContext)
+        .then(result => {
+            result.taggedPDF.saveAsFile(outputTaggedFilePath);
+            result.report.saveAsFile(outputReportFilePath);
+        })
+        .catch(err => {
+            if(err instanceof PDFServicesSdk.Error.ServiceApiError
+                || err instanceof PDFServicesSdk.Error.ServiceUsageError) {
+                console.log('Exception encountered while executing operation', err);
+            } else {
+                console.log('Exception encountered while executing operation', err);
+            }
+        });
+
+    //Generates a string containing a directory structure and file name for the output file.
+    function createOutputFilePath(extension) {
+        let date = new Date();
+        let dateString = date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" +
+            ("0" + date.getDate()).slice(-2) + "T" + ("0" + date.getHours()).slice(-2) + "-" +
+            ("0" + date.getMinutes()).slice(-2) + "-" + ("0" + date.getSeconds()).slice(-2);
+        return ("output/AutotagPDFWithOptions/autotag" + dateString + extension);
+    }
+
+} catch (err) {
+    console.log('Exception encountered while executing operation', err);
+}
---
+++ src/autotag/autotag-pdf.js
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2023 Adobe
+ * All Rights Reserved.
+ *
+ * NOTICE: Adobe permits you to use, modify, and distribute this file in
+ * accordance with the terms of the Adobe license agreement accompanying
+ * it. If you have received this file from a source other than Adobe,
+ * then your use, modification, or distribution of it requires the prior
+ * written permission of Adobe.
+ */
+
+const PDFServicesSdk = require('@dcloud/pdfservices-node-sdk');
+
+/**
+ * This sample illustrates how to generate a tagged PDF.
+ * <p>
+ * Refer to README.md for instructions on how to run the samples.
+ */
+try {
+    // Initial setup, create credentials instance.
+    const credentials =  PDFServicesSdk.Credentials
+        .serviceAccountCredentialsBuilder()
+        .fromFile("pdfservices-api-credentials.json")
+        .build();
+
+    // Create an ExecutionContext using credentials and create a new operation instance.
+    const executionContext = PDFServicesSdk.ExecutionContext.create(credentials),
+        autotagPDF = PDFServicesSdk.AutotagPDF,
+        autotagPDFOperation = autotagPDF.Operation.createNew();
+
+    // Set operation input from a source file.
+    const input = PDFServicesSdk.FileRef.createFromLocalFile('resources/autotagPDFInput.pdf');
+    autotagPDFOperation.setInput(input);
+
+    //Generating a file name
+    let outputTaggedFilePath = createOutputFilePath(".pdf");
+
+    // Execute the operation and Save the result to the specified location.
+    autotagPDFOperation.execute(executionContext)
+        .then(result => {
+            result.taggedPDF.saveAsFile(outputTaggedFilePath);
+        })
+        .catch(err => {
+            if(err instanceof PDFServicesSdk.Error.ServiceApiError
+                || err instanceof PDFServicesSdk.Error.ServiceUsageError) {
+                console.log('Exception encountered while executing operation', err);
+            } else {
+                console.log('Exception encountered while executing operation', err);
+            }
+        });
+
+    //Generates a string containing a directory structure and file name for the output file.
+    function createOutputFilePath(extension) {
+        let date = new Date();
+        let dateString = date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).slice(-2) + "-" +
+            ("0" + date.getDate()).slice(-2) + "T" + ("0" + date.getHours()).slice(-2) + "-" +
+            ("0" + date.getMinutes()).slice(-2) + "-" + ("0" + date.getSeconds()).slice(-2);
+        return ("output/AutotagPDF/autotag" + dateString + extension);
+    }
+
+} catch (err) {
+    console.log('Exception encountered while executing operation', err);
+}
